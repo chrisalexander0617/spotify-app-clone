0537928e95d0cfe54ac71e5c42c2d99335e20660
Chris <44784345+chrisalexander0617@users.noreply.github.com>
2020-10-25 17:08:28 -0400

    Added styling to button and added scopes

--- src/spotify.js
+++ src/spotify.js
@@ -2,3 +2,14 @@
 
 //Handles the authorization for us
 export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+const redirectUri = "http://localhost:3000";
+const clientID = "a339465d3ef7406a8b58bfff5e2e6ee0";
+
+const scopes = [
+    "user-read-currently-playing",
+    "user-read-recently-played",
+    "user-read-playback-state",
+    "user-top-read",
+    "user-modify-playback-state"
+];
